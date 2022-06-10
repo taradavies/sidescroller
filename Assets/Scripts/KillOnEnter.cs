3a8dc9a533abc0ba4fc3a5eccb22800f96ff4579
Tara Davies <tarawafflebear@gmail.com>
2022-06-10 16:51:28 +0800

    Made lava kill the player

---
+++ Assets/Scripts/KillOnEnter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnEnter : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.TryGetComponent<PlayerMovementController>(out var player)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
