d1c1c4d4b398af55c0da4064ec3c20a608c6bd42
Tara Davies <tarawafflebear@gmail.com>
2022-06-10 10:28:46 +0800

    Moved ground check to another class

---
+++ Assets/Scripts/CharacterGrounding.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGrounding : MonoBehaviour
+{
+    [Header("---Ground Check---")]
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _distanceFromGround;
+    [SerializeField] LayerMask _groundMask;
+    public bool IsGrounded { get; private set; }
+
+    void Update() 
+    {
+        UpdateIsGrounded();
+    }
+
+    void UpdateIsGrounded()
+    {
+        var groundSensor = Physics2D.OverlapCircle(_groundCheck.position, _distanceFromGround, _groundMask);
+        IsGrounded = groundSensor != null;
+    }
+}
