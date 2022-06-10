e204e117c0dd028f417666127b32a01b5f2fa1a3
Tara Davies <tarawafflebear@gmail.com>
2022-06-10 10:17:47 +0800

    Added jump

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,19 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [Header("---Movement---")]
     [SerializeField] float _moveSpeed = 20f;
-    Rigidbody2D rb;
-    void Start()
+
+    [Header("Jump")]
+    [SerializeField] float _jumpVelocity;
+
+    [Header("---Ground Check---")]
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _distanceFromGround;
+    [SerializeField] LayerMask _groundMask;
+
+    // private variables
+    bool _isGrounded;
+    Rigidbody2D _rb;
+    float _horizontalInput;
+
+    void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void Update() {
+        _horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontalInput * _moveSpeed, rb.velocity.y);
+        // check if grounded
+        UpdateIsGrounded();
+
+        // move based on user input
+        MoveHorizontal();
+
+        // jumps check
+        if (ShouldStartJump())
+        {
+            Jump();
+        }
+    }
+
+    private void MoveHorizontal()
+    {
+        _rb.velocity = new Vector2(_horizontalInput * _moveSpeed, _rb.velocity.y);
+    }
+
+    private void UpdateIsGrounded()
+    {
+        var groundSensor = Physics2D.OverlapCircle(_groundCheck.position, _distanceFromGround, _groundMask);
+        _isGrounded = groundSensor != null;
+    }
+
+    void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, _jumpVelocity);
+    }
+
+    bool ShouldStartJump()
+    {
+        return Input.GetButton("Jump") && _isGrounded;
     }
 }
