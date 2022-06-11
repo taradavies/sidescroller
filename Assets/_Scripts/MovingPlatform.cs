f0c842f7c092cbceafdee39277f867cc851545e7
Tara Davies <tarawafflebear@gmail.com>
2022-06-11 21:03:40 +0800

    Working moving platform

--- Assets/_Scripts/MovingPlatform.cs
+++ Assets/_Scripts/MovingPlatform.cs
@@ -4,16 +4,14 @@ public class MovingPlatform : Mover
 {
     protected override void CollidedWithPlayer(Collision2D collision)
     {
-        var player = collision.collider.GetComponent<PlayerMovementController>();
-
-        if (transform.position.y < player.transform.position.y)
-            player.transform.SetParent(transform);
+        if (transform.position.y < collision.transform.position.y)
+            collision.transform.SetParent(transform);
     }
 
     void OnCollisionExit2D(Collision2D collision) {
-        if (collision.collider.TryGetComponent<PlayerMovementController>(out var player)) 
+        if (collision.WasHitByPlayer()) 
         {
-            player.transform.SetParent(null);
+            collision.transform.SetParent(null);
         }
     }
 }
